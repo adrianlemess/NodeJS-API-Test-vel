9102b2b303bbea93ee69798d82c7b1ef314588e4
Adrian Lemes <adrianlemess@gmail.com>
2017-02-09 23:14:36 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import config from './config/config';
+import datasource from './config/datasource';
+
+const app = express();
+app.config = config;
+app.datasource = datasource(app);
+app.set('port', 7000);
+const Books = app.datasource.models.Books;
+
+app.route('/books')
+    .get((req, res) => {
+        books.findAll({})
+        .then(result => res.json(result))
+        .catch(err => res.status(412));
+    })
+export default app;
---
+++ models/books.js
@@ -0,0 +1,17 @@
+export default (sequelize, DataType) => {
+    const books = sequelize.define('Books', {
+        id: {
+            type:DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    });
+    return books;
+}
